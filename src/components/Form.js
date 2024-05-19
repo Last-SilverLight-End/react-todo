3d44690bee0ccc4598e8777ccf6b4f96371461f0
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2024-05-19 16:53:12 +0900

    feat: form 구성 구현

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,32 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Form({ onAddTodo }) {
+  const [todo, setTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (todo === '') {
+      return;
+    }
+    onAddTodo(todo);
+    setTodo('');
+  };
+
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
+
   return (
     <div>
-      <input type="text" />
-      <button type="button">추가</button>
+      <input
+        type="text"
+        value={todo}
+        onChange={handleChange}
+        placeholder="할 일을 입력해주세요"
+      />
+      <button type="button" onClick={handleAddTodo}>
+        추가
+      </button>
     </div>
   );
 }
