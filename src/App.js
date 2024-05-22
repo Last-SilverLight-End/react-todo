5ee3b9aaebe124921710654e86549e461aad5e86
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2024-05-22 23:43:11 +0900

    work: input 제작

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useState } from 'react';
 import Form from './components/Form';
 import Item from './components/Item';
 
 function App() {
+  const [todoLists, setTodoLists] = useState([]);
+  // 만약 todolist가 존재 않하면 작동
+  const CheckTodo = () => {};
+  const GetAddTodo = todo => {
+    let check = true;
+    const newID = Math.floor(Math.random() * 100);
+    do {
+      if (todoLists.id.indexof(newID) !== -1) {
+        check = false;
+      }
+    } while (check);
+    setTodoLists([
+      {
+        id: newID,
+        todo,
+        isCompleted: false,
+      },
+      ...todoLists,
+    ]);
+  };
+
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <Form />
+      <Form onAddTodo={GetAddTodo} />
+      {CheckTodo}
       <ul>
-        <Item />
+        {todoLists.map(({ id, todo, isCompleted }) => (
+          <Item
+            key={id}
+            id={id}
+            todo={todo}
+            isCompleted={isCompleted}
+            onDeleteTodo={onDeleteTodo}
+            onCompleteTodo={onCompleteTodo}
+          />
+        ))}
       </ul>
     </div>
   );
